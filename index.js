7be8932c20d20c3021ab700b56303854115928b3
abdulrahman <abdulrahman.omar@student.moringschool.com>
2022-12-01 22:39:55 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// code your solution here
+function superbowlWin(block) {
+  function isWin(element) {
+      return (element["result"] === "W");
+  }
+
+  if (block.find(isWin)) {
+      return block.find(isWin)["year"];
+  } else {
+      return undefined;
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
